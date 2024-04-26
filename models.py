6186eab990c41579d18fca7325f5462250059a17
James Hawk <jph94880@gmail.com>
2024-04-26 14:21:25 -0500

    Adds unique=True constraint to email attribute in User model class

--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class User(db.Model, SerializerMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    email = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     # _password_hash = db.Column(db.String)
